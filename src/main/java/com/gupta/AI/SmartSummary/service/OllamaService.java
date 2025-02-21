97fa350b6c3e7b0558985746e217fa06e0bbbdea
Rajat Gupta <rjt.mangla@gmail.com>
2025-02-21 22:59:14 +0530

    For linkedIn Summarizer

--- src/main/java/com/gupta/AI/SmartSummary/service/OllamaService.java
+++ src/main/java/com/gupta/AI/SmartSummary/service/OllamaService.java
@@ -2,18 +2,36 @@ package com.gupta.AI.SmartSummary.service;
 
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
 public class OllamaService {
     @Autowired
     private OllamaChatModel ollamaChatModel;
 
-    private static final String LINKEDIN_PROMPT=
-            "Summarize the following LinkedIn transcript for study purposes, extracting only key concepts while ignoring unrelated details such as author information or prefaces. Present the summary in a well-structured format for easy reference. Transcript: ";
+    private static final String LINKEDIN_PROMPT =
+            "Summarize the following transcript from linkedin related to the study material, ignoring any unrelated information like author details or prefaces. Present the key concepts in a structured format: ";
 
-    public String summarizeTranscript(String transcript){
-        String prompt=LINKEDIN_PROMPT+" "+transcript;
-        return ollamaChatModel.call(prompt);
+    public String summarizeTranscript(String transcript) {
+        String prompt = LINKEDIN_PROMPT + " " + transcript;
+        String response = ollamaChatModel.call(prompt);
+        return filterForThink(response);
+    }
+
+    /**
+     * This method will be used when we are using deepseek LLM
+     * @param response
+     * @return
+     */
+    private String filterForThink(String response) {
+        if (response.contains("<think>")) {
+            Pattern pattern = Pattern.compile("<think>.*?</think>", Pattern.DOTALL);
+            Matcher matcher = pattern.matcher(response);
+            return matcher.replaceAll("");
+        }
+        return response;
     }
 }
